75468cd9f0b35564adec57bf4e11d39ad91e4ee3
Omar Muhanna <omarmuhomar@hotmail.com>
2020-03-19 18:15:45 -0500

    added main pages

---
+++ src/SignInPage/SignInPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class SignInPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default SignInPage
