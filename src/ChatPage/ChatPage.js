47e1fe57e2063308347a2f7b0388da873e28cc89
Omar Muhanna <omarmuhomar@hotmail.com>
2020-03-23 16:20:54 -0500

    cleaned up chatpage

--- src/ChatPage/ChatPage.js
+++ src/ChatPage/ChatPage.js
@@ -4,9 +4,7 @@ import queryString from 'query-string';
 import io from 'socket.io-client';
 import  './ChatPage.css'
 
-
-
-let socket = ";"
+let socket = ""
 
 export default function ChatPage({ location }) {
     const CONNECTION = 'localhost:5000';
@@ -39,64 +37,3 @@ export default function ChatPage({ location }) {
         </div>
     )
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export class ChatPage extends Component {
-//     render() {
-//         return (
-//             <div>
-//                 <nav>
-//                     <ul className='navbar'>
-//                         <li className='log-out'>
-//                         <Link to="/SignInPage">Log Out</Link> 
-//                              </li>
-//                         <li class='dark-mode'> Enable Dark Mode</li>
-//                     </ul>
-//                 </nav>
-
-//                 <div className='chat-box'>
-//                     <div>
-//                     <p> Chatter1: Whats the weather like today? </p>
-//                     </div>
-
-//                     <div>
-//                     <p> Chatter2: Its warm and sunny. But I would stay inside, you might catch coronavirus. </p>
-//                     </div>
-
-//                     <div>
-//                     <p> Chatter1: Yeah that sounds like a good idea. </p>
-//                     </div>
-
-//                     <form>
-//                     <button  class='chat' type="submit">Chat</button>
-//                     <label for="message">Enter message here:</label><br/>
-//                     <input type="text" id="message" name="message"/><br/>
-
-//                     </form>
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
-
-// export default ChatPage;
