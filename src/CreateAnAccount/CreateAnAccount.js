3725f1652f7e4d45058434c454ba988d364acdf7
Omar Muhanna <omarmuhomar@hotmail.com>
2020-03-26 15:18:37 -0500

    added must match passwords to create account validation

--- src/CreateAnAccount/CreateAnAccount.js
+++ src/CreateAnAccount/CreateAnAccount.js
@@ -5,6 +5,7 @@ import './CreateAnAccount.css'
 function CreateAnAccount() {
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
+    const [matchpassword, setMatchpassword] = useState('');
 
     if(name.length > 5) {
         console.log('Name Must be less than 5 Chars')
@@ -14,17 +15,20 @@ function CreateAnAccount() {
 
     return (
         <div>
+            <form>
             <h1>
                 <h2> Create An Account</h2>
                 <div><input placeholder="User" className="user-box" type="text" onChange={(event) => setName(event.target.value)} ></input></div>
                 <div><input placeholder="Password" className="password-box" type="password" onChange={(event) => setPassword(event.target.value)} ></input> </div>
+                <div><input placeholder="Match Password" className="password-repeat" type="password" onChange={(event) => setMatchpassword(event.target.value)} ></input></div>
                 <Link onClick={event => (!name || !password) ? event.preventDefault() : null} to='/SignInPage'>
-                <button disabled={!name || !password}>
+                <button disabled={!name || !password || password !== matchpassword }>
                  Create Account
                 </button>
                 </Link>
 
             </h1>
+            </form>
         </div>
     )}
 
