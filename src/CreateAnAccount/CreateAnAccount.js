517c4054c4b6aba46a93fcc24a3097686001472d
Omar Muhanna <omarmuhomar@hotmail.com>
2020-03-26 18:51:24 -0500

    small changes to create account

--- src/CreateAnAccount/CreateAnAccount.js
+++ src/CreateAnAccount/CreateAnAccount.js
@@ -2,32 +2,55 @@ import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import './CreateAnAccount.css'
 
-function CreateAnAccount() {
+export default function CreateAnAccount() {
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
     const [matchpassword, setMatchpassword] = useState('');
     const [errors, SetErrors] = useState([]);
 
-    if(name.length <= 3) {
-       
-    }
-
-    // if(name.length > 5) {
-    //     console.log('Name Must be less than 5 Chars')
-    // }
-    // Disable or return error if name is too short
-    //Match passwords!
+// ADD CRUD -> Profiles, admin can delete user
+// Name must be 3 or more charecters otherwise return an error
+// Passwords must match otherwise return an error
+// Password <6 = error, password >20 = error
+// (!password.match(/[0-9]/)) -> or return an error
 
     return (
         <div>
             <form>
             
                 <h2> Create An Account</h2>
-                <div><input placeholder="User" className="user-box" type="text" onChange={(event) => setName(event.target.value)} ></input></div>
-                <div><input placeholder="Password" className="password-box" type="password" onChange={(event) => setPassword(event.target.value)} ></input> </div>
-                <div><input placeholder="Match Password" className="password-repeat" type="password" onChange={(event) => setMatchpassword(event.target.value)} ></input></div>
+
+                <div>
+                    <input 
+                    placeholder="User" 
+                    className="user-box" 
+                    type="text" 
+                    onChange={(event) => setName(event.target.value)} 
+                    ></input>
+                </div>
+
+                <div>
+                    <input 
+                    placeholder="Password" 
+                    className="password-box" 
+                    type="password" 
+                    onChange={(event) => setPassword(event.target.value)} >
+                    </input> 
+                    </div>
+
+                <div>
+                    <input 
+                    placeholder="Match Password" 
+                    className="password-repeat" 
+                    type="password" 
+                    onChange={(event) => setMatchpassword(event.target.value)} >
+                    </input>
+                 </div>
+
+
+
                 <Link onClick={event => (!name || !password) ? event.preventDefault() : null} to='/SignInPage'>
-                <button disabled={!name || !password || password !== matchpassword }>
+                <button disabled={!name || !password}>
                  Create Account
                 </button>
                 </Link>
@@ -37,4 +60,3 @@ function CreateAnAccount() {
         </div>
     )}
 
-export default CreateAnAccount
