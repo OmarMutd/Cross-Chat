cdc4993dc65897807758813f2d97a6a605fa9460
Omar Muhanna <omarmuhomar@hotmail.com>
2020-03-26 19:27:39 -0500

    implemented links for changepassword & delete account

--- src/ManageAccount/ManageAccount.js
+++ src/ManageAccount/ManageAccount.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 // Add date account was created? C [R] UD
 function ManageAccount() {
     return (
         <div>
-           <button to="/DeleteAccount"> Delete Account </button>
-           <button to="/ChangePassword"> Change Password </button>
+           <button> 
+               <Link to="/DeleteAccount">
+                Delete Account
+                </Link> 
+               </button>
+
+               <button> 
+               <Link to="/ChangePassword">
+                Change Password
+                </Link> 
+               </button>
         </div>
     )
 }
