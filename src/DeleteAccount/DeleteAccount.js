078878d0e591aed23f46374a076a30ee20c325ab
Omar Muhanna <omarmuhomar@hotmail.com>
2020-04-02 21:32:35 -0500

    logging delete accoun hook form request

--- src/DeleteAccount/DeleteAccount.js
+++ src/DeleteAccount/DeleteAccount.js
@@ -2,38 +2,51 @@ import React, {useState, useEffect} from 'react'
 import './DeleteAccount.css'
 import config from '../config'
 import { Link } from 'react-router-dom';
-
+import { useForm } from 'react-hook-form';
 
 
 function DeleteAccount() {
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
+    const {register, handleSubmit} = useForm();
 
-    useEffect(()=> {
-        fetch(`${config.API_ENDPOINT}/names/${name}`, {
-            method: 'DELETE',
-            headers: {
-                'content-type': 'application/json',
-                'Authoirization': `Bearer ${config.API_ENDPOINT}`
-            }
-        })
-    })
+    const onSubmit = (data) => {
+        console.log(data);
+    }
+
+    // useEffect(()=> {
+    //     fetch(`${config.API_ENDPOINT}/names/${name}`, {
+    //         method: 'DELETE',
+    //         headers: {
+    //             'content-type': 'application/json',
+    //             'Authoirization': `Bearer ${config.API_ENDPOINT}`,
+    //             'Access-Control-Allow-Origin': 'no-cors'
+    //         }
+    //     })
+    // },)
 
     return (
         <div className='joinOuterContainer'>
             <div className='joinInnerContainer'>
-                <form>
                 <h4 className='header'>Delete Account</h4>
-            <div><input placeholder="User" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} ></input></div>
-            <div><input placeholder="Password" className="joinInput" type="password" onChange={(event) => setPassword(event.target.value)} ></input> </div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                
+            <input ref={register} name='user' placeholder="User" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} ></input>
+            <input ref={register} name='pass' placeholder="Password" className="joinInput" type="password" onChange={(event) => setPassword(event.target.value)} ></input>
 
-            <button className='delete-account-button' disabled={!name || !password}> Confirm Delete Account </button>
-            <Link to='/ManageAccount'><button className='manage-button'> Go Back </button></Link>
-
+            <button type='submit' className='delete-account-button' disabled={!name || !password}> Confirm Delete Account </button>
             </form>
+            <Link to='/ManageAccount'><button className='manage-button'> Go Back </button></Link>
        </div>
         </div>
-    )
+    // <form onSubmit={handleSubmit(onSubmit)}>
+    //     <input type='text' placeholder='Email' name='email' ref={register} ></input>
+    //     <input type='password' placeholder='Password' name='password' ref={register} ></input>
+
+    //     <input type='submit'></input>
+
+    // </form>
+    );
 }
 
 export default DeleteAccount;
