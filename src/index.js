a360e60cb1806d1a50daa5beb12a098951fd74c3
Omar Muhanna <omarmuhomar@hotmail.com>
2020-04-15 18:29:53 -0500

    fixed history bug

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App/App';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+import { Router } from "react-router";
+import history from './history';
+
 
 
 ReactDOM.render(
-<BrowserRouter>
+<Router history={history}>
 <App/>
-</BrowserRouter>,
+</Router>,
 document.getElementById('root'));
 
 
